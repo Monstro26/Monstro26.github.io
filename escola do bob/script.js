c9bbc2ee57187430855850f53ef689fea121120b
Monstro26 <andreygoncalves007@gmail.com>
2025-04-30 12:08:54 -0400

    Add files via upload

---
+++ escola do bob/script.js
@@ -0,0 +1,19 @@
+function mostrarMensagem1() {
+    const mensagem = document.getElementById("Iniciante");
+    mensagem.textContent = "Aqui o foco é aprender o básico com segurança: postura correta, equilíbrio, empurrar o skate e realizar curvas simples.";
+  }
+
+  function mostrarMensagem2() {
+    const mensagem = document.getElementById("Intermediário");
+    mensagem.textContent = "Neste nível, os alunos aprendem manobras básicas como ollie, drop no quarter, e transições suaves nas rampas.";
+  }
+
+  function mostrarMensagem3() {
+    const mensagem = document.getElementById("Avançado");
+    mensagem.textContent = "Os alunos praticam manobras mais técnicas (como flip, grind, nose slide) e ganham autonomia para criar seu próprio estilo.";
+  }
+  
+  function mostrarMensagem4() {
+    const mensagem = document.getElementById("Inscrever");
+    mensagem.textContent = "Obrigado por se inscrever na Escolinha de Skate do Bob!";
+  }
